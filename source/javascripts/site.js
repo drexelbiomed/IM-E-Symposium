6a914d16d1e56df7143997c4643472d7ff837e51
Dave Myers <drm68@drexel.edu>
2019-07-03 15:06:04 -0400

    layout sketch

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,4 +1,46 @@
-ScrollReveal().reveal('.headline', { duration: 2000 });
-ScrollReveal().reveal('.col-sm', { interval: 200 });
+var slideUpSlow = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: 0,
+    duration: 1500,
+    easing: 'ease-out'
+};
 
-console.log('got here')
+var marketingBoxes = {
+  delay: 1500, 
+  interval: 200, 
+  distance:'100%',
+  origin: 'bottom',
+  opacity: 0,
+  duration: 500,
+  easing: 'ease-in'
+};
+
+ScrollReveal().reveal('.headline', slideUpSlow);
+// ScrollReveal().reveal('.headline', { duration: 2000 });
+ScrollReveal().reveal('.col-sm', marketingBoxes);
+
+// var node = document.querySelector('#cake');
+// var nodeList = document.querySelectorAll('.cookies');
+// var nodeArray = [
+//     document.querySelector('#pie'),
+//     document.querySelector('#spoon'),
+//     document.querySelector('#ice-cream')
+// ];
+
+// ScrollReveal().reveal(node);
+// ScrollReveal().reveal(nodeList);
+// ScrollReveal().reveal(nodeArray);
+
+
+// ScrollReveal().reveal('.item', { delay: 250 });
+// ScrollReveal().reveal('.item', { duration: 500 });
+// ScrollReveal().reveal('.item', { delay: 375, reset: true });
+
+// // Is the same as...
+
+// ScrollReveal().reveal('.item', {
+//     delay: 375,
+//     duration: 500,
+//     reset: true
+// });
